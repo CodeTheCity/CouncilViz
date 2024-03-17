6dc3631b0f91bfa4b5b8c53afc5576ee366a729b
Robert McWilliam <rmcw@allmail.net>
2024-03-17 12:01:00 +0000

    Add getting committee members to scraper

--- scraper.py
+++ scraper.py
@@ -14,8 +14,8 @@ class Meeting:
 @dataclass
 class CommMember:
     name: str
-    page_link: str
-    image_link: str
+    page_link: str = ""
+    image_link: str = ""
 
 @dataclass
 class Committee:
@@ -39,12 +39,30 @@ for c in committee_list:
 # for cat, comm in cat_comm.items():
 #     print(f"{cat} {len(comm)} {comm}")
 
+def get_members(link, domain):
+    l = []
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, "html.parser")
+    if m_title := soup.find("a", attrs={"name": "#MEM"}):
+        m_list = m_title.find_next("ul")
+        print(m_list)
+        for m in m_list.find_all("li"):
+            if lnk := m.find("a"):
+                cm = CommMember(lnk.text)
+                cm.page_link = domain+lnk.get("href")
+            else:
+                cm = CommMember(m.text)
+            l.append(cm)
+    return l
+
+# def get_meetings(
+
 start = "https://committees.aberdeencity.gov.uk/mgListCommittees.aspx?bcr=1"
 domain = "https://committees.aberdeencity.gov.uk/"
 r = requests.get(start)
 comm_soup = BeautifulSoup(r.content, "html.parser")
 content_div = comm_soup.find("div", attrs={"class": "mgContent"})
-print(content_div)
+#print(content_div)
 
 committees = []
 for comm in content_div.find_all("li"):
@@ -59,6 +77,10 @@ for comm in content_div.find_all("li"):
         c.category = comm_cat[c.name]
     else:
         c.category = "Other"
+    c.members = get_members(c.link, domain)
     committees.append(c)
+
+for c in committees:
+    print(c)
 
 
