fcc03e65d5d535a74c911de34c9701d0d14d92de
Robert McWilliam <rmcw@allmail.net>
2024-03-17 12:57:42 +0000

    Updates to scraper and outputting json of scraped data

--- scraper.py
+++ scraper.py
@@ -1,15 +1,14 @@
 from collections import defaultdict
 import requests, json
 from bs4 import BeautifulSoup
-from dataclasses import dataclass, field, InitVar
+from dataclasses import dataclass, field, InitVar, asdict
 from typing import List
 
 @dataclass
 class Meeting:
     title: str
-    datetime: str
-    detail_link: str
-    webcast_link: str
+    detail_link: str = ""
+    webcast_link: str = ""
 
 @dataclass
 class CommMember:
@@ -39,13 +38,15 @@ for c in committee_list:
 # for cat, comm in cat_comm.items():
 #     print(f"{cat} {len(comm)} {comm}")
 
+def soupify(link):
+    r = requests.get(link)
+    return BeautifulSoup(r.content, "html.parser")
+    
 def get_members(link, domain):
     l = []
-    r = requests.get(link)
-    soup = BeautifulSoup(r.content, "html.parser")
-    if m_title := soup.find("a", attrs={"name": "#MEM"}):
+    s = soupify(link)
+    if m_title := s.find("a", attrs={"name": "#MEM"}):
         m_list = m_title.find_next("ul")
-        print(m_list)
         for m in m_list.find_all("li"):
             if lnk := m.find("a"):
                 cm = CommMember(lnk.text)
@@ -54,13 +55,26 @@ def get_members(link, domain):
                 cm = CommMember(m.text)
             l.append(cm)
     return l
+def get_meeting_link(s):
+    for lnk in s.find_all("a"):
+        if "Browse meetings and agendas for this committee" in lnk.text:
+            return lnk.get("href")
 
-# def get_meetings(
+def get_meetings(link, domain):
+    meetings = []
+    s = soupify(link)
+    if m_title := s.find("div", {"class": "mgTableTitleTxt"}):
+        m_list = m_title.find_next("ul")
+        for m in m_list.find_all("li"):
+            lnk = m.find("a")
+            metg = Meeting(lnk.text)
+            metg.detail_link = domain+lnk.get("href")
+            meetings.append(metg)
+    return meetings
 
 start = "https://committees.aberdeencity.gov.uk/mgListCommittees.aspx?bcr=1"
 domain = "https://committees.aberdeencity.gov.uk/"
-r = requests.get(start)
-comm_soup = BeautifulSoup(r.content, "html.parser")
+comm_soup = soupify(start)
 content_div = comm_soup.find("div", attrs={"class": "mgContent"})
 #print(content_div)
 
@@ -78,9 +92,13 @@ for comm in content_div.find_all("li"):
     else:
         c.category = "Other"
     c.members = get_members(c.link, domain)
+    c.meetings = get_meetings(domain+get_meeting_link(soupify(c.link)), domain)
     committees.append(c)
+    print(".", end="", flush=True)
+print("")
 
-for c in committees:
-    print(c)
-
+cmt_json = json.dumps([asdict(c) for c in committees], indent=4)
+with open("committees.json", "w") as f:
+    f.write(cmt_json)
+    
 
